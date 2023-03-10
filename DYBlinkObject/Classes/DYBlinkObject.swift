c627a535ca5778679d4e321d199e060f29f77113
doyeonjeong <debby_@kakao.com>
2023-03-10 21:56:23 +0900

    📝 :: update Comments

--- DYBlinkObject/Classes/DYBlinkObject.swift
+++ DYBlinkObject/Classes/DYBlinkObject.swift
@@ -5,12 +5,16 @@ public class DYBlinkObject {
     
     public init() {}
     
-    /// Returns a UIView object with the specified size, corner radius, and background color, and adds a flickering animation to it.
-    /// - Parameters:
-    ///   - width: The width of the view.
-    ///   - height: The height of the view.
-    ///   - radius: The corner radius of the view.
-    /// - Returns: A UIView object with the specified size, corner radius, and background color, and a flickering animation.
+    /**
+     Returns a UIView object with the specified size, corner radius, and background color, and adds a flickering animation to it.
+     
+     - Parameters:
+     - width: The width of the view.
+     - height: The height of the view.
+     - radius: The corner radius of the view.
+     
+     - Returns: A UIView object with the specified size, corner radius, and background color, and a flickering animation.
+     */
     public func drawObject(width: CGFloat, height: CGFloat, radius: CGFloat = 20, color: UIColor = .white) -> UIView {
         let object = UIView()
         object.translatesAutoresizingMaskIntoConstraints = false
@@ -22,7 +26,18 @@ public class DYBlinkObject {
         return object
     }
     
-    // Add a flickering animation to the object
+    /**
+     Adds a blinking animation to the given `UIView` object.
+     
+     - Parameters:
+     - object: The `UIView` object to which the blinking animation will be added.
+     - duration: The duration of the animation.
+     - delay: The delay before the animation starts.
+     - minAlpha: The minimum alpha value for the `UIView`.
+     
+     - Note: This function uses the `UIView.animate(withDuration:delay:options:animations:)` method to create the blinking animation.
+     
+     */
     public func addBlinkingAnimation(to object: UIView, withDuration duration: TimeInterval, delay: TimeInterval, minAlpha: CGFloat) {
         UIView.animate(withDuration: duration, delay: delay, options: [.repeat, .autoreverse]) {
             object.alpha = minAlpha
